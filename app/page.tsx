15fa1edd51e74911146f9ab262a3b520fed30f77
Carlos Rangel <charlesleimaginant@gmail.com>
2024-04-21 00:14:16 -0600

    Creation of PropertyList and PropertyListItem components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
-import Image from "next/image";
+import Categories from "./components/Categories";
+import PropertyList from "./components/properties/PropertyList";
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-airbnb">DjangoBnb</h1>
+    <main className="max-w-[1500px] mx-auto px-6">
+      {/* <h1 className="text-airbnb">DjangoBnb</h1> */}
+      <Categories />
+      
+      <div className="mt-4 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-6">
+          <PropertyList />
+      </div>
     </main>
   );
 }
---
+++ app/properties/[id]/page.tsx
@@ -0,0 +1,84 @@
+import Image from "next/image";
+import Link from "next/link";
+import ReservationSidebar from "@/app/components/properties/ReservationSidebar";
+
+/* import apiService from "@/app/services/apiService";
+import { getUserId } from "@/app/lib/actions"; */
+
+const PropertyDetailPage = async ({ params }: { params: { id: string } }) => {
+  /* const property = await apiService.get(`/api/properties/${params.id}`);
+    const userId = await getUserId(); */
+
+  /* console.log('userId', userId); */
+
+  return (
+    <main className="max-w-[1500px] mx-auto px-6 pb-6">
+      <div className="w-full h-[64vh] mb-4 overflow-hidden rounded-xl relative">
+        <Image
+          fill
+          src="/beach_1.jpg" /* {property.image_url} */
+          className="object-cover w-full h-full"
+          alt="Beach house"
+        />
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-5 gap-4">
+        <div className="py-6 pr-6 col-span-3">
+          <h1 className="mb-4 text-4xl">{/* {property.title} */} Property</h1>
+
+          <span className="mb-6 block text-lg text-gray-600">
+            {/* {property.guests} */}6 guests - {/* {property.bedrooms} */} 2
+            bedrooms - {/* {property.bathrooms} */} 4 bathrooms
+          </span>
+
+          <hr />
+
+          <Link
+            href="/" /* {`/landlords/${property.landlord.id}`} */
+            className="py-6 flex items-center space-x-4"
+          >
+            <Image
+              src="/profile_pic_1.jpg"
+              width={50}
+              height={50}
+              className="rounded-full"
+              alt="The user name"
+            />
+
+            <p>
+              <strong>John</strong> is your host
+            </p>
+            {/* {property.landlord.avatar_url && (
+                            <Image
+                                src={property.landlord.avatar_url}
+                                width={50}
+                                height={50}
+                                className="rounded-full"
+                                alt="The user name"
+                            />
+                        )} */}
+
+            {/* <p><strong>{property.landlord.name}</strong> is your host</p> */}
+          </Link>
+
+          <hr />
+
+          <p className="mt-6 text-lg">Una casa chida</p>
+
+          {/* <p className="mt-6 text-lg">
+                        {property.description}
+                    </p> */}
+        </div>
+
+        <ReservationSidebar property={'property'} userId={'userId'} />
+
+        {/* <ReservationSidebar 
+                    property={property}
+                    userId={userId}
+                /> */}
+      </div>
+    </main>
+  );
+};
+
+export default PropertyDetailPage;
